e0df636bfef9e4a511fb88f8c4dd7e255e07d294
noswanson1982 <noswanson1982@gmail.com>
2021-01-25 17:51:34 -0600

    Delete GeneralMods/StardustCore directory

--- GeneralMods/StardustCore/UIUtilities/MenuComponents/Delegates/Delegates.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace StardustCore.UIUtilities.MenuComponents.Delegates
-{
-    public class Delegates
-    {
-        public delegate void paramaterDelegate(List<object> paramaters);
-    }
-}
--- GeneralMods/StardustCore/UIUtilities/SpriteFonts/Fonts/GenericFont.cs
+++
@@ -1,8 +0,0 @@
-namespace StardustCore.UIUtilities.SpriteFonts.Fonts
-{
-    public class GenericFont
-    {
-        public CharacterSheets.GenericCharacterSheets characterSheet;
-        public string path;
-    }
-}
